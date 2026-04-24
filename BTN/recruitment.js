7403e7ff72bd89cedf983763c52da03ece8fffc8
Vulinh1105 <vul718761@gmail.com>
2026-04-25 04:05:54 +0700

    Update code frontend

--- BTN/recruitment.js
+++ BTN/recruitment.js
@@ -1,6 +1,37 @@
 const toggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
 
 toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  nav.classList.remove("active");
+  overlay.classList.remove("active");
+});const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  nav.classList.remove("active");
+  overlay.classList.remove("active");
+});const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+
+overlay.addEventListener("click", () => {
+  nav.classList.remove("active");
+  overlay.classList.remove("active");
 });
